fd8aecbca01aac6eb0746659e08184b99bd33bd4
türkay ürkmez <turkay.urkmez@outlook.com>
2026-08-24 16:11:14 +0300

    Add CommerceSettings and update configuration files
    Added `CommerceSettings` class to manage app settings. Updated `Program.cs` to configure `CommerceSettings` and added a new `/ayarlar` endpoint. Modified `launchSettings.json` to include a Production profile. Updated `appsettings.json`, `appsettings.Development.json`, and added `appsettings.Production.json` to include `CommerceSettings` and Kestrel configurations.

--- CommerceHub/CommerceHub.Web/Program.cs
+++ CommerceHub/CommerceHub.Web/Program.cs
@@ -3,12 +3,21 @@
 //İstek gelir -> Kestrel dinler -> HttpContext nesnesi oluşturur  -> Geri kalanı backend'in işidir.
 using CommerceHub.Web.Exceptions;
 using CommerceHub.Web.Middleware;
+using CommerceHub.Web.Settings;
+using Microsoft.Extensions.Options;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.Configure<CommerceSettings>(builder.Configuration.GetSection("CommerceSettings"));
 
 var app = builder.Build();
+
+//var value = app.Configuration["CommerceSettings:DefaultCurrency"];
+//Console.WriteLine($"DİKKAT DEĞER: {value}");
+
+
 
 //app.Use(async (context, next) =>
 //{
@@ -26,13 +35,23 @@ app.UseMiddleware<RequestTiminingMiddleware>();
 //app.UseRouting();
 //app.UseAuthentication();//kim? nereye?
 
-
 app.MapGet("/", () => Results.Ok("istek, endpoint'e ulaştı!"));
 app.MapGet("/hata", () =>
 {
 
     throw new NotFoundException("Test verisi bulunamadı!");
-}); 
+});
+app.MapGet("/ayarlar", (IOptions<CommerceSettings> options) =>
+{
+    //TODO 1: Burada, IOptions test edilecek.
+    var settings = options.Value;
+    return Results.Ok(new
+    {
+        settings.DefaultCurrency,
+        settings.MaxOrderItemCount
+
+    });
+});
 app.MapControllers();
 app.Run();
 
---
+++ CommerceHub/CommerceHub.Web/Settings/CommerceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CommerceHub.Web.Settings
+{
+    public class CommerceSettings
+    {
+        public int MaxOrderItemCount { get; set; }
+        public string DefaultCurrency { get; set; } = string.Empty;
+    }
+}
