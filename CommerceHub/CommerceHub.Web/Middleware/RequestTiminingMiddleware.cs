50d89c3bee087940e0d039e58d8909e4417390fc
türkay ürkmez <turkay.urkmez@outlook.com>
2026-08-24 12:29:07 +0300

    Add RequestTimingMiddleware for request timing logs
    Introduced a middleware class `RequestTiminingMiddleware` to measure and log the duration of HTTP requests. Utilized `System.Diagnostics.Stopwatch` to track elapsed time and log the request path, method, and timing details to the console.

---
+++ CommerceHub/CommerceHub.Web/Middleware/RequestTiminingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace CommerceHub.Web.Middleware
+{
+    public class RequestTiminingMiddleware
+    {
+        //kendisinden SONRA başka bir middleware olabilir.
+        private readonly RequestDelegate next;
+        public RequestTiminingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        //peki, middleware'iniz ne yapacak?
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //işlemi yap..
+            var stopwatch = Stopwatch.StartNew();
+            //bir sonraki middleware'e git
+            await next(context);
+            stopwatch.Stop();
+            var elapsed = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"[TIMING] -> {context.Request.Path} adresine gelen {context.Request.Method} isteği, {elapsed} ms sürdü.");
+            //bir sonraki bittikten sonra gerekirse işleme devam et.
+        }
+    }
+}
