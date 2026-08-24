38bacf62f5be039c8077cd6152c9f59b2d463570
türkay ürkmez <turkay.urkmez@outlook.com>
2026-08-24 12:14:33 +0300

    Enable MVC, logging, and routing in web application
    Added `AddControllers` to register controllers with DI. Introduced middleware to log HTTP requests and responses. Configured `MapControllers` for routing and updated the app to build and run using `app.Run()`.

--- CommerceHub/CommerceHub.Web/Program.cs
+++ CommerceHub/CommerceHub.Web/Program.cs
@@ -3,4 +3,22 @@
 //İstek gelir -> Kestrel dinler -> HttpContext nesnesi oluşturur  -> Geri kalanı backend'in işidir.
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+
+    Console.WriteLine($"[İSTEK GELDİ] -> {context.Request.Method} - {context.Request.Path}");
+    await next();
+    Console.WriteLine($"[YANIT ÜRETİLDİ] -> {context.Response.StatusCode}");
+    
+
+});
+
+
+app.MapControllers();
+app.Run();
+
 
